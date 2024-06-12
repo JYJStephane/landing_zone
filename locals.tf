25a83c6080fac1325357b80907ab9c9eec7c2868
root <root@ESWCND2341NX9.es.deloitte.com>
2024-06-12 12:08:22 +0200

    pruebas variables

--- locals.tf
+++ locals.tf
@@ -9,4 +9,13 @@ resource "random_string" "sufijo-s3" {
 # Random suffix for the bucket
 locals {
   s3-sufix = "vpc-flow-logs-${random_string.sufijo-s3.id}"
+
+  cidr = {
+    any        = "0.0.0.0/0"
+    virginia   = "10.10.0.0/16"
+    vpn        = "10.20.0.0/16"
+    public     = "10.10.1.0/24"
+    private    = "10.10.2.0/24"
+    vpn_subnet = "10.20.1.0/24"
+  }
 }
